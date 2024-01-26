f8a5f1189461ca4bcf38ae2530b2f7181510ba73
GeekyCoder1901 <155383219+GeekyCoder1901@users.noreply.github.com>
2024-01-26 22:54:24 +0530

    Add files via upload

---
+++ partition.java
@@ -0,0 +1,13 @@
+Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
+
+class Solution {
+    public int arrayPairSum(int[] nums) {
+       Arrays.sort(nums);
+       int sum=0;
+       for(int i=nums.length-2;i>=0;i=i-2){
+           sum+=nums[i];
+           
+       } 
+       return sum;
+    }
+}
