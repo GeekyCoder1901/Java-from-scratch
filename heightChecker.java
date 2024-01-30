c8d6be23d8f4bd3e4be68b21d814f2469a87da4d
GeekyCoder1901 <155383219+GeekyCoder1901@users.noreply.github.com>
2024-01-30 20:10:02 +0530

    Add files via upload

---
+++ heightChecker.java
@@ -0,0 +1,24 @@
+Height Checker
+A school is trying to take an annual photo of all the students. The students are asked to stand in a single file line in non-decreasing order by height. Let this ordering be represented by the integer array expected where expected[i] is the expected height of the ith student in line.
+
+You are given an integer array heights representing the current order that the students are standing in. Each heights[i] is the height of the ith student in line (0-indexed).
+
+Return the number of indices where heights[i] != expected[i].
+
+class Solution {
+    public int heightChecker(int[] heights) {
+         int count = 0;
+        int[] givenHeights = new int[heights.length];
+        for (int i=0; i< heights.length; i++){
+            givenHeights[i] = heights[i];
+        }
+        Arrays.sort(heights);
+
+        for (int i=0; i<heights.length; i++){
+            if (givenHeights[i] != heights[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
