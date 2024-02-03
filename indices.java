d23e2fe9e13f57d633373370faa033d8db8a438b
GeekyCoder1901 <155383219+GeekyCoder1901@users.noreply.github.com>
2024-02-03 21:23:58 +0530

    Add files via upload

---
+++ indices.java
@@ -0,0 +1,18 @@
+Given an array arr of integers, check if there exist two indices i and j such that :
+
+i != j
+0 <= i, j < arr.length
+arr[i] == 2 * arr[j]
+
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                if((arr[i]==arr[j]*2) && i!=j){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
