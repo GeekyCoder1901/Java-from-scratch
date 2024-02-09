26682e35331acf059c18b451ca19a550be9107cd
GeekyCoder1901 <155383219+GeekyCoder1901@users.noreply.github.com>
2024-02-09 22:12:00 +0530

    Add files via upload

---
+++ overlapping.java
@@ -0,0 +1,14 @@
+You are given a circle represented as (radius, xCenter, yCenter) and an axis-aligned rectangle represented as (x1, y1, x2, y2), where (x1, y1) are the coordinates of the bottom-left corner, and (x2, y2) are the coordinates of the top-right corner of the rectangle.
+
+Return true if the circle and rectangle are overlapped otherwise return false. In other words, check if there is any point (xi, yi) that belongs to the circle and the rectangle at the same time.
+
+class Solution {
+    public boolean checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+         int nearestX = Math.max(x1, Math.min(x2, xCenter));
+        int nearestY = Math.max(y1, Math.min(y2, yCenter));
+
+        int distX = xCenter - nearestX;
+        int distY = yCenter - nearestY;
+        return distX * distX + distY * distY <= radius*radius;
+    }
+}
