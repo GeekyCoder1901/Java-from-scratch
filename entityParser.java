95ee256fbde58afa9f3a14ba01262c943dd19c13
GeekyCoder1901 <155383219+GeekyCoder1901@users.noreply.github.com>
2024-02-12 23:19:26 +0530

    Add files via upload

---
+++ entityParser.java
@@ -0,0 +1,14 @@
+HTML Entity Parser
+
+class Solution {
+    public String entityParser(String text) {
+    text=text.replace("&gt;",">");
+    text=text.replace("&quot;" , "\"");
+    text=text.replace("&apos;", "'");
+    text=text.replace("&lt;" ,"<");
+    text=text.replace("&frasl;","/");
+    text=text.replace("&amp;","&");
+
+return text;
+    }
+}
