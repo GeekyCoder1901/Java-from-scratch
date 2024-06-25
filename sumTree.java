3c0d9e8a3a50042c6cdab044d9b7a3199399ba74
GeekyCoder1901 <155383219+GeekyCoder1901@users.noreply.github.com>
2024-06-25 23:54:53 +0530

    Add files via upload

---
+++ sumTree.java
@@ -0,0 +1,22 @@
+
+Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus the sum of all keys greater than the original key in BST.
+
+As a reminder, a binary search tree is a tree that satisfies these constraints:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+ 
+class Solution {
+    private int sum = 0;
+    
+    public TreeNode bstToGst(TreeNode root) {  
+        if (root != null) {
+            bstToGst(root.right);  // Traverse the right subtree
+            sum += root.val;  // Update the sum
+            root.val = sum;  // Update the current node's value
+            bstToGst(root.left);  // Traverse the left subtree
+        }
+        return root;
+    }
+}
