8db73dad87f17fa6dbb7eb35e02bc9641a716a27
GeekyCoder1901 <155383219+GeekyCoder1901@users.noreply.github.com>
2024-06-30 23:40:11 +0530

    Add files via upload

---
+++ leastNumbers.java
@@ -0,0 +1,19 @@
+Given an array of integers arr and an integer k. Find the least number of unique integers after removing exactly k elements.
+
+class Solution {
+  public int findLeastNumOfUniqueInts(int[] arr, int k) {
+    var map = new HashMap<Integer, Integer>();
+    for (var n : arr)
+      map.put(n, map.getOrDefault(n, 0) + 1);
+    var sortedFreq = new int[map.size()];
+    var i = 0;
+    for (var freq : map.values())
+      sortedFreq[i++] = freq;
+    Arrays.sort(sortedFreq);
+    for (i=0; i < sortedFreq.length; i++) {
+      if (k < sortedFreq[i]) break;
+      k -= sortedFreq[i];
+    }
+    return sortedFreq.length - i;
+  }
+}
