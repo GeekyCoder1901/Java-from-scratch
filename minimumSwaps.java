12fe9fa19bcf073d8ba76a6e721bc63bfa3e5087
GeekyCoder1901 <155383219+GeekyCoder1901@users.noreply.github.com>
2024-08-02 23:51:59 +0530

    Add files via upload

---
+++ minimumSwaps.java
@@ -0,0 +1,23 @@
+A swap is defined as taking two distinct positions in an array and swapping the values in them.
+
+A circular array is defined as an array where we consider the first element and the last element to be adjacent.
+
+Given a binary circular array nums, return the minimum number of swaps required to group all 1's present in the array together at any location.
+
+
+class Solution {
+    public int minSwaps(int[] nums) {
+        int k = Arrays.stream(nums).sum();
+        int n = nums.length;
+        int cnt = 0;
+        for (int i = 0; i < k; ++i) {
+            cnt += nums[i];
+        }
+        int mx = cnt;
+        for (int i = k; i < n + k; ++i) {
+            cnt += nums[i % n] - nums[(i - k + n) % n];
+            mx = Math.max(mx, cnt);
+        }
+        return k - mx;
+    }
+}
