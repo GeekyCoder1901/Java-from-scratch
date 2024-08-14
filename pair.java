784567dc52e8f17e44af62f5133d101feec014e0
GeekyCoder1901 <155383219+GeekyCoder1901@users.noreply.github.com>
2024-08-14 23:38:20 +0530

    Add files via upload

---
+++ pair.java
@@ -0,0 +1,26 @@
+The distance of a pair of integers a and b is defined as the absolute difference between a and b.
+
+Given an integer array nums and an integer k, return the kth smallest distance among all the pairs nums[i] and nums[j] where 0 <= i < j < nums.length
+
+class Solution {
+    public int smallestDistancePair(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int low = 0, high = nums[n - 1] - nums[0];
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (countPairs(nums, mid) < k) low = mid + 1;
+            else high = mid;
+        }
+        return low;
+    }
+    private int countPairs(int[] nums, int maxDistance) {
+        int count = 0, j = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            while (j < nums.length && nums[j] - nums[i] <= maxDistance) ++j;
+            count += j - i - 1;
+        }
+        return count;
+    }
+}
